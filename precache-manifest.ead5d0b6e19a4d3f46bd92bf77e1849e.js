43809f8905d67f677b61af62414ef629eeb881d1
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2019-12-19 15:41:24 +0000

    Updates

---
+++ precache-manifest.ead5d0b6e19a4d3f46bd92bf77e1849e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505ce53d7408d9660ad80b8c62f4a60f",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "182e038f3affa6e26195",
+    "url": "/jokes/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "dcab9ee2c424f00caa14",
+    "url": "/jokes/static/css/main.925d7f66.chunk.css"
+  },
+  {
+    "revision": "182e038f3affa6e26195",
+    "url": "/jokes/static/js/2.a31430a5.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/jokes/static/js/2.a31430a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcab9ee2c424f00caa14",
+    "url": "/jokes/static/js/main.4d1c04bd.chunk.js"
+  },
+  {
+    "revision": "7c366d60acde9b187500",
+    "url": "/jokes/static/js/runtime-main.330b4d0e.js"
+  }
+]);
